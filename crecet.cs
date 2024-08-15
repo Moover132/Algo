29d8ecec9a943264c9d03b55b842acc42132d73e
Moover132 <79909611+Moover132@users.noreply.github.com>
2024-08-15 16:00:49 +0400

    Add files via upload

---
+++ crecet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crecet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //Печатает крикет 
+        print("crickets");
+    }
+}
